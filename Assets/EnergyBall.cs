658a8e91ca2aaa0093d6e384035ac059667494b7
Stewart Grant <grant-stewart@hotmail.com>
2018-10-07 18:58:22 +1100

    Latest Version. Should be up to date.

---
+++ Assets/EnergyBall.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnergyBall : MonoBehaviour {
+    //Who Shot the Projectile. I shouldn't hit the caster regardless if they touch it.
+    public GameObject Shooter;
+    public int ShooterNum = 0;
+    void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.tag == "Player" && collision.gameObject != Shooter)
+        {
+            Destroy(gameObject);
+        }
+        if (collision.gameObject.tag == "Floor")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/ArmourBoost.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArmourBoost : MonoBehaviour {
+
+    public float ArmourGain = 25;
+    public float RespawnTimeMax = 32;
+    public bool Heavy = false;
+
+    private float RespawnTime = 12;
+    private Vector3 StartPosition;
+    // Use this for initialization
+    void Start()
+    {
+        StartPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (RespawnTime < RespawnTimeMax)
+        {
+            RespawnTime += 1 * Time.deltaTime;
+        }
+        else
+        {
+            transform.position = StartPosition;
+            RespawnTime = RespawnTimeMax;
+        }
+    }
+    void OnTriggerStay(Collider collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            transform.position = new Vector3(9990.0f, 9999.0f, 9990.0f);
+            RespawnTime = 0;
+        }
+    }
+}
